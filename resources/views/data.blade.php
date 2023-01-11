1d8ee188d139342ed7f32a3a8a55bed9e7aa4f53
Ridwan RI1 <ridwanacer58@gmail.com>
2023-01-11 21:58:42 +0700

    (+)extendYield tampilan data

--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -1,22 +1,13 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    @extends('template.main')
+@section('title', 'create produk')
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
+@section('content') 
     <div class="container">
       <h1 class="text-center mb-4 text-primary">Daftar Produk</h1>
       <div class="table">
         <a href="/Create" class="btn btn-success mb-4">Tambah Data</a>
-       
-        <table class="table">
+        <table class="table ">
           <thead>
             <tr>
               <th scope="col">#</th>
@@ -30,9 +21,9 @@
             @foreach ($product as $pr)
             <tr>
               <th scope="row">1</th>
+              <td class="w-25"><img src="{{ Storage::url($pr->image) }}" class="w-25"></td>
               <td>{{$pr->name }}</td>
               <td>{{$pr->description}}</td>
-              <td><img src="{{ Storage::url($pr->image) }}" class="w-50"></td>
               <td>
                 <a href="/edit/{{$pr->id}}" type="button" class="btn btn-primary">Edit</a>
                 <a href="{{ route ('delete',[ "id" => $pr->id]) }}" type="button" class="btn btn-danger" >Delete</a>
@@ -44,14 +35,4 @@
         </table>
       </div>
     </div>
-  
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
-  </body>
-</html>
+  @endsection
