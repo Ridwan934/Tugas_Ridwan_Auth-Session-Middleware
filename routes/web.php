ec7006441c492d1092fae84bd3f7206e8f6c4716
Ridwan RI1 <ridwanacer58@gmail.com>
2023-01-11 22:03:45 +0700

    (+) route edit update dan store

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ use App\Http\Controllers\AuthController;
 
 Route::get('/', [productController::class, 'index'])->name('data');
 Route::get('/Create', [productController::class, 'create'])->middleware(["withAuth"]);
-Route::get('/edit/{{id}}', [productController::class, 'edit']);
-Route::put('/update/{{id}}', [productController::class, 'update']);
-Route::post('/store', [productController::class, 'Store'])->middleware(["noAuth"]);
+Route::get('/edit/{id}', [productController::class, 'edit']);
+Route::put('/update/{id}', [productController::class, 'update']);
+Route::post('/store', [ProductController::class, 'Store'])->middleware(["withAuth"]);
 Route::get('/delete/{id}', [productController::class, 'destroy'])->name('delete');
 route::any("/login", [AuthController::class, "login"])->name('login') ->middleware(["noAuth"]);
 route::any("/logout", [AuthController::class, "logout"])->name('logout') ->middleware(["withAuth"]);
